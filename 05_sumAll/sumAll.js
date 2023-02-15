7da0a013739e96f3485513a505f8578eed689e17
simon-upton <simon@uptonhome.com>
2023-02-14 17:10:21 -0800

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+  let finalSum = 0;
 
+  if ((int1 < 0 || int2 < 0) ||
+      (typeof int1 != "number" || typeof int2 != "number")
+  ) {
+    return "ERROR";
+  };
+
+  if (int1 > int2) {
+    for (let i = int2; i <= int1; i++) {
+      finalSum += i;
+    }
+  } else {
+    for (let i = int1; i <= int2; i++) {
+      finalSum += i;
+    }
+  }
+
+
+
+  return finalSum;
 };
 
 // Do not edit below this line
