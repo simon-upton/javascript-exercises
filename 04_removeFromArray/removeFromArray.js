577c8dc6604670f5534c045598157e8d5dd57da0
simon-upton <simon@uptonhome.com>
2023-02-14 16:54:44 -0800

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    for (target of targets) {
+        arr = arr.filter(function(item) {
+            return !(item === target);
+        })
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
