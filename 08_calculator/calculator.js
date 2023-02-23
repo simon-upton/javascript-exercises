ce9d5a3ea543180d7cc9d693ec037a31137ec659
simon-upton <simon@uptonhome.com>
2023-02-22 16:54:17 -0800

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (arr.length == 0) return 0;
 
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let final = 1;
+
+  for (let i = 1; i <= num; i++) {
+    final *= i;
+  }
+
+  return final;
 };
 
 // Do not edit below this line
