01521b9380a227365a2002a42edbc20960135ea0
simon-upton <simon@uptonhome.com>
2023-02-22 17:17:22 -0800

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = str.toLowerCase().replace(/[ .,!]/g, '');
+    let reverseStr = cleanStr.split('').reverse().join('');
 
+    return cleanStr == reverseStr;
 };
 
 // Do not edit below this line
